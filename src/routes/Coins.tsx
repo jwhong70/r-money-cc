c890a8d1e9ceb51318a86142276846c324352c52
[jwhong70] <[jwhong70@gmail.com]>
2022-11-28 15:32:03 +0900

    #2-2 Coins Screen Layout

--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -1,4 +1,28 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  padding: 0px 20px;
+  max-width: 480px;
+  margin: 0 auto;
+`;
+const Header = styled.header`
+  height: 15vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+const Title = styled.h1`
+  font-size: 48px;
+  color: ${(props) => props.theme.accentColor};
+`;
+
 function Coins() {
-  return null;
+  return (
+    <Container>
+      <Header>
+        <Title>Coin</Title>
+      </Header>
+    </Container>
+  );
 }
 export default Coins;
