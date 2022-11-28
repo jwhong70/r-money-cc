ec08555769e48b32908685b3ec64325598464708
[jwhong70] <[jwhong70@gmail.com]>
2022-11-28 17:41:41 +0900

    #3-1 Coin Screen Layout

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,7 +1,35 @@
 import { useParams } from "react-router-dom";
+import { useLocation } from "react-router";
+import styled from "styled-components";
+
+const Container = styled.div`
+  padding: 0px 20px;
+  max-width: 480px;
+  margin: 0 auto;
+`;
+const Header = styled.header`
+  height: 15vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+const Title = styled.h1`
+  font-size: 48px;
+  color: ${(props) => props.theme.accentColor};
+`;
+interface RouteState {
+  state: { name: string };
+}
 
 function Coin() {
   const { coinId } = useParams();
-  return null;
+  const { state } = useLocation() as RouteState;
+  return (
+    <Container>
+      <Header>
+        <Title></Title>
+      </Header>
+    </Container>
+  );
 }
 export default Coin;
