da211c6ba95dcadb488db64ed9eb1a4ebbc08f3f
[jwhong70] <[jwhong70@gmail.com]>
2022-11-28 16:06:17 +0900

    #2-3 React-query

--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -1,4 +1,6 @@
 import styled from "styled-components";
+import { useQuery } from "@tanstack/react-query";
+import { fetchCoins } from "../api";
 
 const Container = styled.div`
   padding: 0px 20px;
@@ -15,13 +17,28 @@ const Title = styled.h1`
   font-size: 48px;
   color: ${(props) => props.theme.accentColor};
 `;
-
+const Loader = styled.span`
+  text-align: center;
+  display: block;
+`;
+const CoinsList = styled.ul``;
+interface ICoin {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  is_new: boolean;
+  is_active: boolean;
+  type: string;
+}
 function Coins() {
+  const { isLoading, data } = useQuery<ICoin[]>(["allCoins"], fetchCoins);
   return (
     <Container>
       <Header>
         <Title>Coin</Title>
       </Header>
+      {isLoading ? <Loader>Loading...</Loader> : <CoinsList></CoinsList>}
     </Container>
   );
 }
